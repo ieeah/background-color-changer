3bf5fbf52578283f379c9ba6f50bb0b99793ffcd
ieeah <87276064+ieeah@users.noreply.github.com>
2022-06-23 20:01:49 +0200

    refactor

--- main.js
+++ main.js
@@ -1,64 +1,65 @@
 const colorList = [
-    'red',
-    '#000',
-    '#fcba03',
-    '#6f9bab',
-    'rgb(239, 199, 240)',
-    'dodgerblue',
-    'yellow',
-    'rgb(255, 255, 255)'
+  "red",
+  "#000",
+  "#fcba03",
+  "#6f9bab",
+  "rgb(239, 199, 240)",
+  "dodgerblue",
+  "yellow",
+  "rgb(255, 255, 255)",
+  "#00bd55",
 ];
 
-const hexComposer =
-[
-    '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'A', 'B', 'C', 'D', 'E', 'F'
+const hexComposer = [
+  "0",
+  "1",
+  "2",
+  "3",
+  "4",
+  "5",
+  "6",
+  "7",
+  "8",
+  "9",
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
 ];
 
-const btnList = document.querySelector('.btn-list');
-const btnHex = document.querySelector('.btn-hex');
-const btnRgb = document.querySelector('.btn-rgb');
-const hexLi = document.getElementById('hexLi');
-const simpleLi = document.getElementById('simpleLi');
-const rgbLi = document.getElementById('rgbLi');
+const sections = document.querySelectorAll("section");
 let bgColor = 0;
 let hxRandom = 0;
 const bodyColor = document.body.style.backgroundColor;
-const colorTextlist = document.getElementById('bghere1');
-const colorTextHex = document.getElementById('bghere2');
-const colorTextRgb = document.getElementById('bghere3');
+const colorTextlist = document.getElementById("bghere1");
+const colorTextHex = document.getElementById("bghere2");
+const colorTextRgb = document.getElementById("bghere3");
 
+const tabs = ["0", "1", "2"];
+let activeTab = "0";
 
-hexLi.addEventListener('click', function() {
-    document.querySelector('.hex.d-none').classList.remove('d-none');
-    document.querySelector('.list').classList.add('d-none');
-    document.querySelector('.rgb').classList.add('d-none');
-});
+function setActiveTab(tab) {
+  activeTab = tab.toString();
+}
+function setActiveSection(index) {
+  sections.forEach((section, i) => {
+    if (i != index) {
+      section.classList.add("d-none");
+    } else {
+      section.classList.remove("d-none");
+    }
+  });
+}
 
-simpleLi.addEventListener('click', function() {
-    
-    document.querySelector('.list.d-none').classList.remove('d-none');
-    document.querySelector('.hex').classList.add('d-none');
-    document.querySelector('.rgb').classList.add('d-none');
-});
+function troughList() {
+  bgColor >= colorList.length - 1 ? (bgColor = 0) : bgColor++;
+  document.body.style.backgroundColor = colorList[bgColor];
+  colorTextlist.innerText = `${colorList[bgColor]}`;
+}
 
-rgbLi.addEventListener('click', function() {
-
-    document.querySelector('.rgb.d-none').classList.remove('d-none');
-    document.querySelector('.hex').classList.add('d-none');
-    document.querySelector('.list').classList.add('d-none');
-});
-
-btnList.addEventListener('click', function () {
-
-
-    bgColor >= 7 ? bgColor = 0 : bgColor++;
-    document.body.style.backgroundColor = colorList[bgColor];
-    colorTextlist.innerText = ` ${colorList[bgColor]} ${bgColor}`;
-
-});
-
-
-btnHex.addEventListener('click', function() {
+function generateHEX() {
   let hexColorNew = "#";
 
   for (let i = 0; i < 6; i++) {
@@ -68,16 +69,14 @@ btnHex.addEventListener('click', function() {
 
   document.body.style.backgroundColor = hexColorNew;
   colorTextHex.innerText = `${hexColorNew}`;
-});
+}
 
-btnRgb.addEventListener('click', function () {
-    let rgb1 = Math.floor(Math.random() * 256);
-    let rgb2 = Math.floor(Math.random() * 256);
-    let rgb3 = Math.floor(Math.random() * 256);
-    let rgbColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
+function generateRGB() {
+  let rgb1 = Math.floor(Math.random() * 256);
+  let rgb2 = Math.floor(Math.random() * 256);
+  let rgb3 = Math.floor(Math.random() * 256);
+  let rgbColor = `rgb(${rgb1}, ${rgb2}, ${rgb3})`;
 
-    document.body.style.backgroundColor = rgbColor;
-    colorTextRgb.innerText = `${rgbColor}`;
-
-    console.log(rgbColor);
-});
+  document.body.style.backgroundColor = rgbColor;
+  colorTextRgb.innerText = `${rgbColor}`;
+}
